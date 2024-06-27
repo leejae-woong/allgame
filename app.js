c1c5ddfa6559578195a06aa93ae6185d76db53a5
leejae-woong <leejaewong0629@gmail.com>
2024-06-27 18:03:47 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+document.getElementById('game1').addEventListener('click', function() {
+    window.open('https://leejae-woong.github.io/horsegame/');
+});
+
+document.getElementById('game2').addEventListener('click', function() {
+    window.open('https://leejae-woong.github.io/littlegame/');
+});
+
+document.getElementById('game3').addEventListener('click', function() {
+    window.open('https://leejae-woong.github.io/beangame/');
+});
